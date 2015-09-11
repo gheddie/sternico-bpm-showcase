c44f35d10e5ce5eb1208c15d1f4c20303a4647d9
Stefan.Schulz <Stefan.Schulz@MD1FB0AC.ww009.siemens.net>
2015-09-11 21:59:32 +0200

    third commit

--- bpm-showcase/src/main/java/de/sternico/bpm/service/BpmEngineAccessor.java
+++ bpm-showcase/src/main/java/de/sternico/bpm/service/BpmEngineAccessor.java
@@ -4,14 +4,11 @@ import javax.jws.WebService;
 import javax.jws.soap.SOAPBinding;
 import javax.jws.soap.SOAPBinding.Style;
 
+import org.camunda.bpm.BpmPlatform;
+import org.camunda.bpm.engine.repository.ProcessDefinition;
+
 /**
- * + http://maksim.sorokin.dk/it/2011/01/20/jax-ws-web-services-maven-tomcat/ (!!!!!!!!!!!!!!!!!!!!!)
- * + http://memorynotfound.com/deploy-jaxws-application-tomcat-example/
- * + http://ics.upjs.sk/~novotnyr/blog/2068/deploying-jax-ws-services-on-java-7-and-tomcat-7
- * + http://www.java2blog.com/2013/03/jaxws-webservice-deployement-on-tomcat.html
- * + http://www.mkyong.com/webservices/jax-ws/jax-ws-hello-world-example-document-style/
- * + http://www.dineshonjava.com/2013/06/restful-web-services-with-jersey-jax-rs.html#.VfG9OVWqqko
- * + http://poor-developer.blogspot.de/2014/02/jersey-25-maven-easy-rest-web-services.html
+ * + http://maksim.sorokin.dk/it/2011/01/20/jax-ws-web-services-maven-tomcat/
  * 
  * @author Stefan.Schulz
  *
@@ -21,9 +18,21 @@ import javax.jws.soap.SOAPBinding.Style;
 @SOAPBinding(style = Style.RPC)
 public class BpmEngineAccessor
 {
-    public String printMessage()
+    public void correlateMessage(String messageName, String businessKey)
     {
-        System.out.println("123");
-        return "123";
+        BpmPlatform.getDefaultProcessEngine().getRuntimeService().correlateMessage(messageName, businessKey);
+    }
+    
+    public void startProcessInstanceByKey(String processDefinitionKey)
+    {
+        BpmPlatform.getDefaultProcessEngine().getRuntimeService().startProcessInstanceByKey(processDefinitionKey);
+    }
+    
+    public void debugProcessDefinitionKeys()
+    {
+        for (ProcessDefinition processDefinition : BpmPlatform.getDefaultProcessEngine().getRepositoryService().createProcessDefinitionQuery().list())
+        {
+            System.out.println("deployed key : " + processDefinition.getKey());
+        }
     }
 }
--- bpm-ws-client/src/main/java/de/sternico/bpm/BpmEngineClient.java
+++ bpm-ws-client/src/main/java/de/sternico/bpm/BpmEngineClient.java
@@ -1,9 +1,21 @@
 package de.sternico.bpm;
 
+import de.sternico.bpm.service.BpmEngineAccessor;
+import de.sternico.bpm.service.BpmEngineAccessorService;
+
+/**
+ * wsimport -keep -verbose http://localhost:8080/bpm-showcase-0.0.1-SNAPSHOT/BpmEngineAccessorWs?wsdl
+ * 
+ * @author Stefan.Schulz
+ *
+ */
 public class BpmEngineClient
 {
     public static void main(String[] args)
     {
-        System.out.println("123");
+        BpmEngineAccessor accessor = new BpmEngineAccessorService().getBpmEngineAccessorPort();
+        accessor.startProcessInstanceByKey("LibraryRequestUserProcess");
+//        accessor.correlateMessage("MSG_LIC_AVAIL");
+//        accessor.debugProcessDefinitionKeys();
     }
 }
